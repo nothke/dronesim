489187ac34e5b90b3b45987df29c1849f1315508
nothke <nothke@gmail.com>
2025-09-20 16:42:43 +0200

    Sokol window working

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,51 @@
 const std = @import("std");
+const sokol = @import("sokol");
+const slog = sokol.log;
+const sg = sokol.gfx;
+const sglue = sokol.glue;
+
+const state = struct {
+    var pass_action: sg.PassAction = .{};
+};
+
+export fn init() void {
+    sg.setup(.{
+        .environment = sglue.environment(),
+        .logger = .{ .func = slog.func },
+    });
+
+    state.pass_action.colors[0] = .{
+        .load_action = .CLEAR,
+        .clear_value = .{ .r = 1, .g = 1, .b = 0, .a = 1 },
+    };
+
+    std.debug.print("Backend: {}\n", .{sg.queryBackend()});
+}
+
+export fn frame() void {
+    const g = state.pass_action.colors[0].clear_value.g + 0.01;
+    state.pass_action.colors[0].clear_value.g = if (g > 1.0) 0.0 else g;
+
+    sg.beginPass(.{ .action = state.pass_action, .swapchain = sglue.swapchain() });
+    sg.endPass();
+    sg.commit();
+}
+
+export fn cleanup() void {
+    sg.shutdown();
+}
 
 pub fn main() !void {
     std.debug.print("Hello drones!\n", .{});
+
+    sokol.app.run(.{
+        .init_cb = init,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+        .width = 800,
+        .height = 600,
+        .icon = .{ .sokol_default = true },
+        .window_title = "DroneSim",
+        .logger = .{ .func = slog.func },
+    });
 }
