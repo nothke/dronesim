dee198d1690c0446a65be76d9441b569e60619f6
nothke <nothke@gmail.com>
2025-09-20 16:23:50 +0200

    Added zig-sokol

--- build.zig
+++ build.zig
@@ -2,16 +2,22 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
+
+    const dep_sokol = b.dependency("sokol", .{ .target = target, .optimize = optimize });
 
     const exe = b.addExecutable(.{
         .name = "dronesim",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
-
             .target = target,
             .optimize = optimize,
+            .imports = &.{
+                .{
+                    .name = "sokol",
+                    .module = dep_sokol.module("sokol"),
+                },
+            },
         }),
     });
 
