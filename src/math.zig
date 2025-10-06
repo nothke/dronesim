5bfb25308ba86f5b4bde16ef8c38ef61abeb7e8a
nothke <nothke@gmail.com>
2025-10-06 03:21:35 +0200

    Default init vectors to 0 for easier partial init

--- src/math.zig
+++ src/math.zig
@@ -14,8 +14,8 @@ fn radians(deg: f32) f32 {
 }
 
 pub const Vec2 = extern struct {
-    x: f32,
-    y: f32,
+    x: f32 = 0,
+    y: f32 = 0,
 
     pub fn zero() Vec2 {
         return Vec2{ .x = 0.0, .y = 0.0 };
@@ -27,9 +27,9 @@ pub const Vec2 = extern struct {
 };
 
 pub const Vec3 = extern struct {
-    x: f32,
-    y: f32,
-    z: f32,
+    x: f32 = 0,
+    y: f32 = 0,
+    z: f32 = 0,
 
     pub fn zero() Vec3 {
         return Vec3{ .x = 0.0, .y = 0.0, .z = 0.0 };
